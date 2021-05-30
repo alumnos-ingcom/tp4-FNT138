cd792523ca742caa6660b85c68f636ebe55539a0
Fede Trujillo <fede4895@gmail.com>
2021-05-30 20:02:25 -0300

    Modificación tp4ej2
    Se modificaron las observaciones que hizo el profesor:
    Se cambio la interacción con el usuario a la función prueba

--- Funciones/tp4ej2.py
+++ Funciones/tp4ej2.py
@@ -3,32 +3,36 @@
 # Plantilla de ejercicio
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
-from tp4ej1 import IngresoIncorrecto
+from tp4ej1 import IngresoIncorrecto, ingreso_entero
 
 def suma_lenta(numero, otro_numero):
-    try:
-        numero = int (input("Ingrese Un numero: # "))
-        otro_numero = int (input("Ingrese Otro numero: # "))
-        suma = ""
-    except ValueError as err:
-        raise IngresoIncorrecto ("Valor No Valido") from err
-    resultado = (f"={numero+otro_numero}")
-    
-    if otro_numero > 0:
-        while otro_numero > 0:
+    """
+    Suma Dos Numeros Enteros Sin Hacer La Operacion Directa
+    ej:
+        4 + 2 = 4+1+1
+    """
+    num1 = numero
+    num2 = otro_numero
+    suma = " "
+        
+    if  num2 > 0:
+        while num2 > 0:
             suma = suma+"+1"
-            otro_numero = otro_numero-1
-        return str(numero)+suma+ resultado
+             num2 = num2 - 1
+        return suma
     
     else:
-        while otro_numero < 0:
+        while num2 < 0:
             suma = suma+"-1"
-            otro_numero = otro_numero +1
-        return str(numero) + suma+ resultado    
+            num2 = num2 + 1
+        return suma   
 
 def prueba():
-    num = suma_lenta( 0, 0)
-    print(num)
+    numero = ingreso_entero("Ingrese un numero: ")
+    otro_numero = ingreso_entero("Ingrese otro numero: ")
+    a_sumar = suma_lenta(numero, otro_numero)
+    
+    print(f"{numero}{a_sumar} = {numero+otro_numero}")
 
 if __name__ == "__main__":
     prueba()
